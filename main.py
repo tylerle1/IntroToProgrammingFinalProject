9449ca82bdfa8e1b4ff8c4241f8f469889784cf3
tylerle1 <115042850+tylerle1@users.noreply.github.com>
2022-11-28 09:34:19 -0800

    created list, init pygame, and created screen

--- main.py
+++ main.py
@@ -3,3 +3,30 @@
 # import libraries
 import pygame
 import random
+
+
+# game settings
+FPS = 30
+HEIGHT = 600
+WIDTH = 800
+
+# colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# create list of words
+sports = ['soccer', 'basketball']
+
+animals = ['zebra', 'dog']
+
+people = ['Michael Jordan']
+
+# initiate pygame and create a window
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Game loop
+running = True
+while running:
+    screen.fill(WHITE)
+
